7fafaf941282e025fbc09b46c49f8dfe06691588
janginbeom <inbeom8742@naver.com>
2022-06-29 18:59:36 +0900

    spring security config is added.

--- spring-boot/src/main/java/springboot/Application.java
+++ spring-boot/src/main/java/springboot/Application.java
@@ -2,9 +2,15 @@ package springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    UserDetailsServiceAutoConfiguration.class,
+    SecurityAutoConfiguration.class,
+})
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
